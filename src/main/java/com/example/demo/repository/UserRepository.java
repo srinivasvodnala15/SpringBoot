e757016f3b47b481ab01819fd8520f510c8fec19
srinivasvodnala15 <srinivasvodnala15@gmail.com>
2024-11-20 21:50:08 -0600

    new endpoint to get users

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+public interface UserRepository extends JpaRepository<Users, Integer> {
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Users;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    public UserRepository userRepository;
+
+    public List<Users> getUsers(){
+        return userRepository.findAll();
+    }
+}
